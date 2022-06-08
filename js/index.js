dd34ace1940dca595986f6e4c66869593b20d088
unknown <Tanvir Ahmed>
2022-06-08 21:19:31 +0600

    testimonial section added

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true,
+            },
+            600: {
+                items: 2,
+                nav: true,
+            },
+            1000: {
+                items: 3,
+                nav: false,
+                loop: true,
+                dots: true,
+                autoplay: true,
+                autoplaySpeed: 1500,
+            },
+        },
+    });
+});
