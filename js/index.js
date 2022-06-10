30d2ef240a7064fe686191290ae5299a6bebee26
unknown <Tanvir Ahmed>
2022-06-10 15:37:40 +0600

    counter added

--- js/index.js
+++ js/index.js
@@ -33,3 +33,27 @@ $(document).ready(function () {
 });
 
 AOS.init();
+const counterUp = window.counterUp.default;
+
+const callback = (entries) => {
+    entries.forEach((entry) => {
+        const el = entry.target;
+        if (entry.isIntersecting && !el.classList.contains("is-visible")) {
+            counterUp(el, {
+                duration: 1500,
+                delay: 10,
+            });
+            el.classList.add("is-visible");
+        }
+    });
+};
+
+const IO = new IntersectionObserver(callback, { threshold: 1 });
+const counterClasses = [".counter", ".counter2", ".counter3", ".counter4"];
+
+for (const cls of counterClasses) {
+    const el = document.querySelector(cls);
+    IO.observe(el);
+}
+// const bl = document.querySelector(".counter2");
+// IO.observe(bl);
